e71c9bb66f8a3b225f2078adb96e40413e5b1bcd
Geetaprakash <86463646+Geetaprakash@users.noreply.github.com>
2021-09-28 20:28:18 +0530

    To calculate Harmonic Number

---
+++ HarmonicNum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class HarmonicNum{
+	public static void main(String[] args){
+		double sum=0;
+		Scanner num=new Scanner(System.in);
+		double n= num.nextDouble();
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+(1/i);
+			if(i==1)
+				System.out.println("1 + ");
+			else
+				System.out.println("1/"+ i);
+		}
+	System.out.println();
+	System.out.println("sum of the num is:"+sum);
+}
+}
+			
