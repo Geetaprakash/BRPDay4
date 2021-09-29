b7511b755960da1df5827fb71410aef10a350afc
Geetaprakash <86463646+Geetaprakash@users.noreply.github.com>
2021-09-29 07:36:15 +0530

    Program to convert temprature

---
+++ TempCon.java
@@ -0,0 +1,9 @@
+public class TempCon {
+        public static void main (String args[])
+        {
+            float Fahre, Celsi;
+            Fahre = 4;
+            Celsi  = ((Fahre-3)*1)/2;
+            System.out.println("Temp in cel is: "+Celsi);
+        }
+}
